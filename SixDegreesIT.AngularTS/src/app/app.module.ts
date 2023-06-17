b34005f44a31a3d3f448544a2f51059e70da5502
larry-noriega <larry.noriega@hotmail.com>
2023-06-17 09:54:42 -0500

    build: Front End Connection.

--- SixDegreesIT.AngularTS/src/app/app.module.ts
+++ SixDegreesIT.AngularTS/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListPersonsComponent } from './components/list-persons/list-persons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPersonsComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
--- SixDegreesIT.AngularTS/src/proxy.conf.js
+++ SixDegreesIT.AngularTS/src/proxy.conf.js
@@ -2,6 +2,7 @@ const PROXY_CONFIG = [
   {
     context: [
       "/weatherforecast",
+      "/api/persons"
     ],
     target: "https://localhost:7239",
     secure: false
